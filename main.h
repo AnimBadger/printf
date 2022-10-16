0a5e25b317b795bcde05daefc46714ebe5accc0a
Provoski <provo.iyen27@gmail.com>
2022-10-16 22:34:40 +0000

    _putchar function and _puts functiona was added. 0-printf.c which is an empty file was removed

--- main.h
+++ main.h
@@ -1,7 +1,42 @@
 #ifndef __MAIN__
 #define __MAIN__
 
-int _putchar(char c);
+/* libraries header */
+#include <unistd.h>
+
+/**
+* _putchar -  user output function for character
+* Description: it is used to print character output to "stdio"
+* @c: input type of variable need of type "char"
+* Return: char c
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _puts - user output function for strings
+ * Description: it is used to print string output to "stdio"
+ * @str: input type of vsriable need of type
+ * Return: char str
+ */
+int _puts(char *str)
+{
+	int index;
+	int count;
+
+	count = 0;
+	index = 0;
+	while (str[index] && str[index] != '\0')
+	{
+		_putchar(str[index]);
+		index++;
+		count++;
+	}
+	return (count);
+}
+
 int _printf(const char *format, ...);
 
 #endif
