8bdff8b14d2f7f4cf0d4dc080e8100c5c92b63a4
Provoski <provo.iyen27@gmail.com>
2022-10-16 19:14:38 -0700

    _putchar.c renamed to print_functions.c, _strlen function added to lenght_functions.c file

---
+++ lenght_functions.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - count lenght of a string
+ * @str: Parameter passed to function
+ * Return: lenght of string
+ */
+
+int _strlen(char *str)
+{
+	int count;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
