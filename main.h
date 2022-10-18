bf631d92a27c99b02941ea47d7ce778419c8cefa
Provoski <provo.iyen27@gmail.com>
2022-10-18 15:03:26 -0700

    prototype of _print_mod function was added to main.h

--- main.h
+++ main.h
@@ -1,17 +1,20 @@
 #ifndef __MAIN__
 #define __MAIN__
 
+/* standard library header file */
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
 
+/* functions prototype */
 int _puts(va_list s);
 int _putchar(va_list chr);
 int _printf(const char *format, ...);
+int _print_mod(va_list mod);
 
 /**
- * struct spec - structure that define specifiers
- * @:id: id specifier
+ * spec - structure that define specifiers
+ * @:id: case identifier
  * @fptr: function pointer
  */
 
